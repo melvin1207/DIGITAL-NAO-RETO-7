360e78c68f6e8abdb83aaf41dedeca5374b9f78e
Melvin <melsava1207@gmail.com>
2024-08-12 12:53:23 -0600

    se cambio archivo eslint y se añadio el prefijo principal de la API

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/biblioteca');
   await app.listen(3000);
 }
+
 bootstrap();
