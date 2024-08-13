ea9f63f31b07dcefceef0ab5502e7eb21d904c4b
Melvin <melsava1207@gmail.com>
2024-08-12 19:56:47 -0600

    Archivos de libros creados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LibrosService } from './libros/libros.service';
+import { LibrosController } from './libros/libros.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, LibrosController],
+  providers: [AppService, LibrosService],
 })
 export class AppModule {}
