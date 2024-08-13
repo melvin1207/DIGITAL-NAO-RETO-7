685745ee169f543c9581638ab27626ffa7b963a5
Melvin <melsava1207@gmail.com>
2024-08-13 08:48:14 -0600

    Todos los servicios para libros creados, falta cambiar la logica

--- src/libros/libros.controller.ts
+++ src/libros/libros.controller.ts
@@ -1,12 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { LibrosService } from './libros.service';
 
 @Controller('libros')
 export class LibrosController {
   constructor(private librosService: LibrosService) {}
 
+  @Post()
+  crearLibro() {
+    return this.librosService.crearLibro();
+  }
+
   @Get()
-  findAll() {
-    return this.librosService.findAll();
+  obtenerTodos() {
+    return this.librosService.obtenerTodos();
+  }
+
+  @Get()
+  obtenerLibro() {
+    return this.librosService.obtenerLibro();
+  }
+
+  @Patch()
+  actualizarLibro() {
+    return this.librosService.actualizarLibro();
+  }
+
+  @Patch()
+  activarLibro() {
+    return this.librosService.activarLibro();
+  }
+
+  @Delete()
+  desactivarLibro() {
+    return this.librosService.desactivarLibro();
+  }
+
+  @Delete()
+  eliminarLibro() {
+    return this.librosService.eliminarLibro();
   }
 }
--- src/libros/libros.service.ts
+++ src/libros/libros.service.ts
@@ -2,7 +2,25 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class LibrosService {
-  findAll(): any {
-    return 'finAll listo';
+  crearLibro(): any {
+    return 'Crear Libro';
+  }
+  obtenerTodos(): any {
+    return 'Obtener todos listo';
+  }
+  obtenerLibro() {
+    return 'obtener libro listo';
+  }
+  actualizarLibro() {
+    return 'actualizar libro listo';
+  }
+  activarLibro() {
+    return 'activar libro listo';
+  }
+  desactivarLibro() {
+    return 'desactivar libro listo';
+  }
+  eliminarLibro() {
+    return 'eliminar libro listo';
   }
 }
