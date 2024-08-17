7cc0c6e9c4d2c52b6abf9c3bbc5322ecb3fe6af9
Melvin <melsava1207@gmail.com>
2024-08-17 14:29:14 -0600

    App principal comentada

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,27 @@
+//Se exportan las librerias necesarias para el funcionamiento
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+//importación de los modelos de las tablas
 import { LibrosModule } from './libros/libros.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { FavoritosModule } from './favoritos/favoritos.module';
+
+//se importa el modelo de autenticación del JWT
 import { AuthModule } from './auth/auth.module';
 
+//se importa dotenv para manejo de variables de entorno
 import * as dotenv from 'dotenv';
-
 dotenv.config();
 
+//Aplicación principal
 @Module({
   imports: [
-    LibrosModule,
-    UsuariosModule,
-    FavoritosModule,
+    LibrosModule, //Manejara los endpoints de libro
+    UsuariosModule, //Manejara los endpoints de usuarios
+    FavoritosModule, //Manejara los endpoints de Favoritos
+
+    //Se crea la conexión con la base de datos en PostgresSQL
     TypeOrmModule.forRoot({
       type: 'postgres',
 
@@ -25,7 +33,7 @@ dotenv.config();
       entities: ['dist/**/*.entity.js'],
       synchronize: true,
     }),
-    AuthModule,
+    AuthModule, //Modulo para verificar el JWT
   ],
 })
-export class AppModule {}
+export class AppModule {} //Se exporta para su uso
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
+//si importan las librerias necesarias para el funcionamiento de la aplicación
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module'; //Se importa el modulo de App
 
+//se crea una función bootstrap para inicializar la aplicación Nestjs
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/biblioteca');
-  await app.listen(3000);
+  app.setGlobalPrefix('api/biblioteca'); //prefijo universal
+  await app.listen(3000); //puerto de despliegue
 }
 
+//se ejecuta la función bootstrap, para la aplicación
 bootstrap();
